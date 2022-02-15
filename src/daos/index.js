5c7e6eee73ac43f0748b5c2b6ffb97ee566ef94c
EduardoVelarde <55457194+EduardoVelarde@users.noreply.github.com>
2022-02-15 10:17:47 -0600

    format changes
    format changes

--- src/daos/index.js
+++ src/daos/index.js
@@ -2,11 +2,11 @@ let productDao
 let cartsDao
 switch('mongo'){
     case 'json':
-        const { default:DaoCartFiles}=await import('./carts/DaoCartsFiles.js')
-        const { default:DaoProductsFiles}=await import('./products/DaoProductsFiles.js')
-        productDao= new DaoProductsFiles()
-        cartsDao=new DaoCartFiles()
-        break
+            const { default:DaoCartFiles}=await import('./carts/DaoCartsFiles.js')
+            const { default:DaoProductsFiles}=await import('./products/DaoProductsFiles.js')
+            productDao= new DaoProductsFiles()
+            cartsDao=new DaoCartFiles()
+            break
         case 'mongo':
             const { default:DaoCartsMongo}=await import('./carts/DaoCartMongo.js')
             const { default:DaoProductsMongo}=await import('./products/DaoProductsMongo.js')
