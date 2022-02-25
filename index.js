d9d107a545c6c1759886927b52bbc7fd4d21b0dc
EduardoVelarde <55457194+EduardoVelarde@users.noreply.github.com>
2022-02-25 00:50:19 -0600

    last changes to production

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ if (modeCluster&&cluster.master) {
     }
 } else {
     const app = express()
-    const PORT = 8080
+    const PORT = process.env.PORT||8080
     app.use(express.json())
     app.use(express.urlencoded({ extended: false }))
 
